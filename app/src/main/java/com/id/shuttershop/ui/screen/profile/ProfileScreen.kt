47cf1e2340cfa4e22307e6ab79f22eff1a44c923
Andre Eka Putra Simanjuntak <70775081+andremoore123@users.noreply.github.com>
2024-08-11 16:47:52 +0700

    [ADJUSTMENT] Adjustment Profile Feature - Show Message Below Language Switch if not supported - Update Indonesian String Value

--- app/src/main/java/com/id/shuttershop/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/id/shuttershop/ui/screen/profile/ProfileScreen.kt
@@ -1,5 +1,6 @@
 package com.id.shuttershop.ui.screen.profile;
 
+import android.os.Build
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +22,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -45,11 +48,11 @@ fun ProfileScreen(
     val isDarkMode by viewModel.isDarkMode.collectAsState()
     val isIndonesiaLanguage by viewModel.isIndonesiaLanguage.collectAsState()
     val userData by viewModel.userData.collectAsState()
+    val languageFeatureEnabled = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
 
     LaunchedEffect(key1 = Unit) {
         viewModel.fetchUserData()
     }
-
 
     ProfileContent(
         modifier = modifier,
@@ -59,13 +62,15 @@ fun ProfileScreen(
         isIndonesia = isIndonesiaLanguage,
         onThemeSwitchChange = viewModel::setDarkMode,
         onLanguageSwitchChange = viewModel::setIndonesiaLanguage,
-        onLogoutClick = viewModel::logout
+        onLogoutClick = viewModel::logout,
+        languageFeatureEnabled = languageFeatureEnabled
     )
 }
 
 @Composable
 internal fun ProfileContent(
     modifier: Modifier = Modifier,
+    languageFeatureEnabled: Boolean,
     profileName: String = "",
     profileEmail: String = "",
     profileImageUrl: String = "",
@@ -116,12 +121,28 @@ internal fun ProfileContent(
                 onCheckChange = onThemeSwitchChange,
                 checked = isDarkMode
             )
-            SwitchWithText(
-                firstText = stringResource(R.string.text_en),
-                secondText = stringResource(R.string.text_id),
-                onCheckChange = onLanguageSwitchChange,
-                checked = isIndonesia
-            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                SwitchWithText(
+                    firstText = stringResource(R.string.text_en),
+                    secondText = stringResource(R.string.text_id),
+                    onCheckChange = onLanguageSwitchChange,
+                    checked = isIndonesia,
+                    enabled = languageFeatureEnabled
+                )
+                if (languageFeatureEnabled.not()) {
+                    Text(
+                        modifier = Modifier.padding(top = 10.dp),
+                        text = stringResource(R.string.text_version_not_supported),
+                        style = MaterialTheme.typography.labelSmall.copy(
+                            color = DividerDefaults.color
+                        ),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
         }
         PrimaryButton(
             text = stringResource(R.string.text_logout),
@@ -135,6 +156,6 @@ internal fun ProfileContent(
 @Preview(showBackground = true, showSystemUi = true)
 internal fun ShowProfileScreenPreview() {
     ShutterShopTheme {
-        ProfileContent()
+        ProfileContent(languageFeatureEnabled = false)
     }
 }
